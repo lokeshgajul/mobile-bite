01e83409ebe801aa38fd28fec74ac1eab93f6c3a
lokeshgajul <sangeetagajul@gmail.com>
2023-12-23 14:58:23 +0530

    Initial commit

---
+++ frontend/src/components/Filters.js
@@ -0,0 +1,232 @@
+import React, { useState } from "react";
+
+const Filters = ({ setFilteredMobiles, setSearchTerm }) => {
+  const [selectedPriceRange, setSelectedPriceRange] = useState("");
+  const [mobileName, setMobileName] = useState("");
+  const [mobileProcessor, setMobileProcessor] = useState("");
+  const [mobileDetails, setMobileDetails] = useState(null);
+  const [selectedMemory, setSelectedMemory] = useState("");
+
+  const handleFilterByMemory = async (event) => {
+    const Memory = event.target.value;
+
+    setSelectedMemory(Memory);
+    setMobileProcessor("");
+    setMobileName("");
+
+    setSelectedMemory(Memory);
+    try {
+      const response = await fetch("http://localhost:8000/api/filterByMemory", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ selectedMemory: Memory }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setFilteredMobiles(data.mobiles);
+      } else {
+        console.error("Error filtering mobiles:", response.statusText);
+        setFilteredMobiles([]);
+      }
+    } catch (error) {
+      console.error("Error filtering mobiles:", error);
+      setFilteredMobiles([]);
+    }
+  };
+
+  const handleFilterByName = async () => {
+    setMobileProcessor(""); // Reset mobileProcessor when name filter is applied
+    setSelectedMemory("");
+
+    try {
+      const response = await fetch("http://localhost:8000/api/filterByName", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ mobileName }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setMobileDetails(data);
+      } else {
+        console.error("Error filtering mobiles:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error filtering mobiles:", error);
+    }
+  };
+
+  const handleFilterByProcessor = async (event) => {
+    const processor = event.target.value;
+
+    setMobileProcessor(processor);
+    setMobileName("");
+    setSelectedMemory("");
+    if (processor) {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/filterByProcessor`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ mobileProcessor: processor }),
+          }
+        );
+
+        const data = await response.json();
+
+        if (data.mobiles && Array.isArray(data.mobiles)) {
+          setFilteredMobiles(data.mobiles);
+        } else {
+          console.error("Mobiles not found in the selected price range");
+          setFilteredMobiles([]);
+        }
+      } catch (error) {
+        console.error("Error fetching mobiles:", error);
+        setFilteredMobiles([]);
+      }
+    } else {
+      // Handle the case when no price range is selected (show all mobiles)
+      setFilteredMobiles([]);
+    }
+  };
+
+  const handlePriceRangeChange = async (event) => {
+    const priceRange = event.target.value;
+
+    setSelectedPriceRange(priceRange);
+    setMobileName("");
+    setMobileProcessor("");
+    setSelectedMemory("");
+
+    if (priceRange) {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/filterByPrice`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ filteredPrice: priceRange }),
+          }
+        );
+
+        const data = await response.json();
+
+        if (data.mobiles && Array.isArray(data.mobiles)) {
+          setFilteredMobiles(data.mobiles);
+        } else {
+          console.error("Mobiles not found in the selected price range");
+          setFilteredMobiles([]);
+        }
+      } catch (error) {
+        console.error("Error fetching mobiles:", error);
+        setFilteredMobiles([]);
+      }
+    } else {
+      setFilteredMobiles([]);
+    }
+  };
+
+  const handleNameChange = (event) => {
+    const name = event.target.value;
+    setMobileName(name);
+    setSearchTerm(name);
+    setMobileProcessor("");
+    setSelectedMemory("");
+  };
+
+  return (
+    <div className="py-3 max-md:flex flex-col">
+      <h2 className="pb-5">Filters</h2>
+      <label htmlFor="priceRange">Price Range:</label>
+      <div className="flex flex-col my-2">
+        <select
+          className="max-w-[70%] py-1"
+          id="priceRange"
+          name="priceRange"
+          value={selectedPriceRange}
+          onChange={handlePriceRangeChange}
+        >
+          <option value="">Select Price</option>
+          <option value="10000-20000">10,000 - 20,000</option>
+          <option value="20001-30000">20,001 - 30,000</option>
+          <option value="30001-40000">30,001 - 40,000</option>
+          <option value="40001-50000">40,001 - 50,000</option>
+          <option value="50001">50,000 and above</option>
+        </select>
+        <p className="py-2">
+          selected range: {selectedPriceRange ? selectedPriceRange : "all "}
+        </p>
+      </div>
+
+      <div className="flex flex-col py-3">
+        <select
+          className="max-w-[70%] py-1"
+          id="priceRange"
+          name="priceRange"
+          value={mobileProcessor}
+          onChange={handleFilterByProcessor}
+        >
+          <option value="">Select Processor</option>
+          <option value="Snapdragon">Snapdragon </option>
+          <option value="Google Tensor">Google Tensor</option>
+          <option value="MediaTek Helio G95">MediaTek Helio G95</option>
+          <option value="Exynos">Exynos </option>
+          <option value="A15 Bionic">A15 Bionic</option>
+        </select>
+        <p className="py-2">
+          selected processor : {mobileProcessor ? mobileProcessor : "all "}
+        </p>
+      </div>
+
+      <div>
+        <input
+          className="border-2 border-black rounded-md bg-blue-200 mr-2 p-1.5"
+          type="text"
+          value={mobileName}
+          onChange={handleNameChange}
+          placeholder="Search mobile here..."
+        />
+        <button onClick={handleFilterByName}>Filter by Name</button>
+
+        {mobileDetails && (
+          <div>
+            <h2>{mobileDetails.name}</h2>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-col py-3">
+        <select
+          className="max-w-[70%] py-1"
+          id="memoryDropdown"
+          name="memoryDropdown"
+          value={selectedMemory}
+          onChange={handleFilterByMemory}
+        >
+          <option>Select Memory</option>
+          <option value="4GB 64GB">4GB 64GB</option>
+          <option value="8GB 128GB">8GB 128GB</option>
+          <option value="16GB 256GB">16GB 256GB</option>
+          {/* Add more options as needed */}
+        </select>
+        <p className="py-2" onClick={handleFilterByMemory}>
+          {selectedMemory
+            ? `Selected Memory ${selectedMemory}`
+            : "Filter by RAM and Memory "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
---
+++ frontend/src/components/MobileDetailsPage.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const MobileDetails = () => {
+  const { mobileId } = useParams();
+  const [mobileDetails, setMobileDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchMobileDetails = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/mobiles/${mobileId}`
+        );
+        const data = await response.json();
+
+        if (data.mobile) {
+          setMobileDetails(data.mobile);
+        } else {
+          console.error("Mobile details not found");
+        }
+      } catch (error) {
+        console.error("Error fetching mobile details:", error);
+      }
+    };
+
+    fetchMobileDetails();
+  }, [mobileId]);
+
+  if (!mobileDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div className="grid max-md:grid-cols-1 md:grid-cols-3 p-5">
+        <div className="flex justify-center flex-col items-center">
+          <img
+            className="col-span-1 max-w-lg  h-auto rounded-md"
+            src={mobileDetails.image}
+            alt={mobileDetails.name}
+          />
+          <div className="flex m-6">
+            <button className="bg-blue-400 hover:bg-blue-500 hover:text-white rounded-lg p-3 m-2 max-w-sm">
+              Add To Cart
+            </button>
+            <button className="bg-blue-400 hover:bg-blue-500 hover:text-white rounded-lg p-3 m-2 max-w-sm">
+              Buy Now
+            </button>
+          </div>
+        </div>
+
+        <div className=" col-span-2 p-9">
+          <div className="flex flex-col justify-evenly items-stretch text-lg">
+            <h2 className="font-semibold text-4xl ">{mobileDetails.name}</h2>
+            <p className="pt-2 text-2xl text-blue-500">
+              {mobileDetails.ratingsAndReviews}
+            </p>
+            <p className="text-gray-700 text-2xl font-semibold py-3">
+              Price: ₹ {mobileDetails.price}
+            </p>
+            <p className="text-gray-700 ">
+              <span className="font-semibold"> Type: </span>
+              {mobileDetails.type}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold"> battery:</span>{" "}
+              {mobileDetails.battery}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold"> camera:</span>{" "}
+              {mobileDetails.camera}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold"> Processor:</span>{" "}
+              {mobileDetails.processor}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold"> displaySize:</span>{" "}
+              {mobileDetails.displaySize}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold"> Memory: </span>
+              {mobileDetails.memory}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold"> OS:</span> {mobileDetails.os}
+            </p>
+
+            <p className="text-gray-700">
+              <span className="font-semibold"> Description: </span>
+              {mobileDetails.description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileDetails;
